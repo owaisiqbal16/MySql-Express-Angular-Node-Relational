e32bd496be208ed985cb82ac9ff0cd40e8bd1301
owaisiqbal16 <m.owaisiqbal16@gmail.com>
2019-09-14 18:14:03 +0500

    Connected node server with angular using CORS

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {CatsComponent } from './cats/cats.component';
+import {CatDetailComponent} from './cat-detail/cat-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'cats' , component: CatsComponent },
+  { path : 'cats/:id' , component : CatDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule }    from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CatsComponent } from './cats/cats.component';
+import { CatDetailComponent } from './cat-detail/cat-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CatsComponent
+    CatsComponent,
+    CatDetailComponent
   ],
   imports: [
     BrowserModule,
